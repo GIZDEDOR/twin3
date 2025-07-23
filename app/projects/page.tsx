ae97a338c3f4dff1b16948309077278a772e890d
GIZDEDOR <igorek-rybalchenko@mail.ru>
2025-07-24 01:40:09 +0300

    initial

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,21 @@
-import ProjectsClient from '@/components/projects/ProjectsClient'
-import Footer from '@/components/Footer'
+
+
+// Принудительно SSR и без кэша
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
+import ProjectsClient from '@/components/projects/ProjectsClient';
+import Footer from '@/components/Footer';
 import { createClient } from '@/prismicio';
 
 export default async function ProjectsPage() {
+  // Просто создаём клиент без previewData
   const client = createClient();
+
+  // Берём страницу слайсов
   const page = await client.getSingle('projects');
   const slices = page.data.slices;
+
 
   return (
     <>
